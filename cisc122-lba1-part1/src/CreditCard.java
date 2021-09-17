7ebab6c1464eeeaf185ef4df158e986a7d4bba12
Ryan Jones <ryan.jones@disneystreaming.com>
2021-09-17 11:28:04 -0400

    part 1 qos change

--- cisc122-lba1-part1/src/CreditCard.java
+++ cisc122-lba1-part1/src/CreditCard.java
@@ -1,7 +1,7 @@
 // This class looks up credit card balances
 public class CreditCard {
 	private Money balance;
-	private Money creditLimit; 
+	private Money creditLimit;
 	private Person owner;
 
 	/**
@@ -12,10 +12,9 @@ public class CreditCard {
 	 */
 	public CreditCard(Person newCardHolder, Money limit) {
 		balance = new Money(0); // initialize the balance to 0
-		creditLimit = new Money(0); // initialize credit limit to 0. 
+		creditLimit = new Money(0); // initialize credit limit to 0.
 		this.creditLimit = limit;
 		this.owner = newCardHolder;
-		
 
 	}
 
@@ -48,27 +47,31 @@ public class CreditCard {
 		return new Money(creditLimit);
 
 	}
-/*
- * @return owner.toString
- */
+
+	/*
+	 * @return owner.toString
+	 */
 	public String getPersonals() {
 		return owner.toString();
 	}
- 
-	/* add to the balance every time something is charged  only if the balance is not greater than the credit limit
+
+	/*
+	 * add to the balance every time something is charged only if the balance is not
+	 * greater than the credit limit
 	 * 
 	 */
 	public void charge(Money amount) {
-		if ((balance.getDollars() + balance.getCents())+ (amount.getDollars() + amount.getCents()) >= (creditLimit.getDollars() + creditLimit.getCents())) {
+		if ((balance.getDollars() + amount.getDollars())
+				+ (balance.getCents() + amount.getCents()) >= (creditLimit.getDollars() + creditLimit.getCents())) {
 			System.out.println(" Exceeds credit limit");
-		}
-		else {
-			System.out.println( "Charge:" + " " + amount);
+		} else {
+			System.out.println("Charge:" + " " + amount);
 			balance = balance.add(amount);
 		}
 	}
 
-	/* subtract from balance when a payment is made
+	/*
+	 * subtract from balance when a payment is made
 	 * 
 	 */
 	public void payment(Money amount) {
