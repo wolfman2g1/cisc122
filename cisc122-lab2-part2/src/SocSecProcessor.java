cd5e0b63bd0116ff8218f9589e1df755babcecf9
wolfman2g1 <ryan.jones26@gmail.com>
2021-10-06 23:17:13 -0400

    lab2 part2 mostly working, need to figure out the loop to start over

--- cisc122-lab2-part2/src/SocSecProcessor.java
+++ cisc122-lab2-part2/src/SocSecProcessor.java
@@ -5,44 +5,42 @@ public class SocSecProcessor {
 	public static void main(String[] args) throws SocSecException {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		
+
 		try {
 			System.out.println(" Please enter a name:" + "\n");
 			String name = sc.nextLine();
-			
+
 			System.out.println(); // insert newline
 			System.out.println(" Please enter a ssd:" + "\n");
 			String ssn = sc.nextLine();
+			boolean isValidSsn = isValid(ssn);
+			if (isValidSsn) {
+				System.out.println(name + " " + ssn + " " + "is Valid");
+				System.out.println(" Continue ?");
+				String again = sc.nextLine();
+
 			}
-		catch (SocSecException e) {
+		} catch (SocSecException e) {
 			System.out.println(e.getMessage());
-			
+
 		}
-		
+		sc.close();
 
 	}
 
-	public static boolean isName(String name) {
-		boolean check = true;
-		if ( name.length() == 0) {
-			System.out.println("You must enter a name");
-			check = false;
-			
-		}
-		return check;
-		
-	}
-	public static boolean isValid(String ssn) throws SocSecException  {
+
+	public static boolean isValid(String ssn) throws SocSecException {
 		boolean check;
 		if (ssn.length() < 11) {
 			check = false;
+			throw new SocSecException("not a vaild social security number!");
 		} else if (!ssn.matches("^[0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9][0-9][0-9]$")) {
 			throw new SocSecException("not a vaild social security number!");
 
 		} else {
 			check = true;
 		}
-		return check;
+		return true;
 	}
 
 }
