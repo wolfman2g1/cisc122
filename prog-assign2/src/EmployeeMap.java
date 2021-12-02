cda1eee668f858bbe7642cf35fb364f212b6153a
wolfman2g1 <ryan.jones26@gmail.com>
2021-12-02 15:52:24 -0500

    testing

--- prog-assign2/src/EmployeeMap.java
+++ prog-assign2/src/EmployeeMap.java
@@ -2,14 +2,25 @@ import javax.swing.*;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
 public class EmployeeMap {
-  public Map currentMap =
   public static void main(String args[]){
-    Employee e1 = new Employee(119930, "ted");
+      String file;
+      try {
+          file = JOptionPane.showInputDialog("Enter file path");
+          Scanner sc = new Scanner(new BufferedReader(new FileReader(file)));
+          while (sc.hasNext()){
+              Employee e = new Employee( sc.nextLine().split(","));
+
+          }
+      }
+      catch (IOException e){
+
+      }
 
   }
 
