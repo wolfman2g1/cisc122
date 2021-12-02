2af814420778ad3272dc7a582a550665f4f923b9
wolfman2g1 <ryan.jones26@gmail.com>
2021-12-02 13:39:23 -0500

    new stuf

--- prog-assign2/src/Employee.java
+++ prog-assign2/src/Employee.java
@@ -1,16 +1,13 @@
-import java.util.HashMap;
-import java.util.Map;
-
 public class Employee {
     protected Integer id;
-    protected String name;
+    protected Employee name;
 
     /**
      * Constructor to create employee
      * @param n
      * @param i
      */
-    public Employee (String n, int i){
+    public Employee (Employee n, int i){
         this.name = n;
         this.id = i;
 
@@ -18,11 +15,7 @@ public class Employee {
     public Employee(){
 
     }
-    private Map<Integer,String> mappedEmployee(Employee e) {
-        Map <Integer, String> employeeMap = new HashMap<>();
-        employeeMap.put(e.id,e.name);
-        return employeeMap;
-    }
+
 
 
 }
--- prog-assign2/src/EmployeeMap.java
+++ prog-assign2/src/EmployeeMap.java
@@ -1,7 +1,30 @@
+import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
 public class EmployeeMap extends Employee {
-  public static void main(String args[]){
-      
+  public static void main(String args[]) throws Exception{
+      String file;
+      try {
+          file = JOptionPane.showInputDialog("Enter file path");
+          Scanner sc = new Scanner(new BufferedReader(new FileReader(file)));
+          while (sc.hasNext()){
+           String[] mapped = sc.nextLine().split(",");
+          }
+      }
+      catch (IOException e){
+
+      }
+
   }
+    public Map<Integer,Employee> mappedEmployee(Employee e) {
+        Map <Integer, Employee> employeeMap = new HashMap<>();
+        employeeMap.put(e.id,e.name);
+        return employeeMap;
+    }
 
 }
