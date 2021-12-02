14bed7ab4222229002a8ca8979b9134881eaf891
Ryan Jones <ryan.jones26@gmail.com>
2021-12-02 12:20:11 -0500

    Update EmployeeMap.java

--- prog-assign2/src/EmployeeMap.java
+++ prog-assign2/src/EmployeeMap.java
@@ -1,7 +1,14 @@
 
-public class EmployeeMap extends Employee {
+public class EmployeeMap  {
   public static void main(String args[]){
+    Employee e1 = new Employee(119930, "ted");
       
   }
+  
+  private Map<Integer,Employee> mappedEmployee(Employee e) {
+        Map <Integer, Employee> employeeMap = new HashMap<>();
+        employeeMap.put(e.id,e.name);
+        return employeeMap;
+    }
 
 }
