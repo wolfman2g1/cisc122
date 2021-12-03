c5465dc9020666696ea82029ca4233bd3d7ad081
wolfman2g1 <ryan.jones26@gmail.com>
2021-12-02 20:52:55 -0500

    got the search partially working.

--- prog-assign2/src/Driver.java
+++ prog-assign2/src/Driver.java
@@ -2,27 +2,54 @@ import javax.swing.*;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.InputMismatchException;
+import java.util.Map;
 import java.util.Scanner;
 
+/**
+ * test harnes
+ */
 public class Driver {
-    public static void main(String args[]){
+    public static void main(String args[]) {
         String file;
-        try {
+        try { // get file from user
             file = JOptionPane.showInputDialog("Enter file path");
             Scanner sc = new Scanner(new BufferedReader(new FileReader(file)));
-            while (sc.hasNext()){
+            while (sc.hasNext()) {
                 int id = sc.nextInt();
                 String name = sc.next();
                 Employee e = new Employee(name, id);
                 EmployeeMap eM = new EmployeeMap();
                 eM.insert(e);
-                System.out.println(eM.getEmployee(e.name).toString());
+                // System.out.println(eM.getEmployee(e.name).toString());
+                String search = JOptionPane.showInputDialog("Search for a name");
+                if (!search.matches("[a-zA-Z]*")) { // check for string
+                    throw new InputMismatchException();
+                } else {
 
+                    Employee s1 = eM.getEmployee(search);
+                    if (s1.name.equals(search)) {
+                        JOptionPane.showMessageDialog(null, "Found" + " " + s1.name + " " + "ID" + " " + s1.id);
+                    } else {
+                        JOptionPane.showMessageDialog(null, search + " " + " Not Found!");
+                    }
+
+
+                }
             }
+        } catch (IOException e) {
+            e.getMessage();
         }
-        catch (IOException e){
-
+        catch (InputMismatchException e) {
+            JOptionPane.showMessageDialog(null,  " Names only please!");
+        }
+        catch (NullPointerException e){
+            System.exit(0);
         }
 
     }
+
+
 }
+
+
--- prog-assign2/src/EmployeeMap.java
+++ prog-assign2/src/EmployeeMap.java
@@ -1,11 +1,5 @@
-import javax.swing.*;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Scanner;
 
 public class EmployeeMap {
   Map <String, Employee> employeeMap = new HashMap<>();
